de04d8c231b26bf1ee470e1675d3d087359530e8
chris-brugal <chrisbrugal@yahoo.com>
2020-08-06 11:46:16 -0400

    Removed internal testing data

--- src/app.js
+++ src/app.js
@@ -20,14 +20,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'water bill', amount: 100, createdAt: 1000}));
-store.dispatch(addExpense({description:'gas bill', amount: 400, createdAt: -1000}));
-store.dispatch(addExpense({description:'rent', amount: 109500, createdAt: 4500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter ></AppRouter>
