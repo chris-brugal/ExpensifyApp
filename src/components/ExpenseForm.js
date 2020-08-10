3dd5ac8ece8870962a1a404a69fbe669226e5c80
chris-brugal <chrisbrugal@yahoo.com>
2020-08-09 20:52:09 -0400

    Added styling

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,11 +57,11 @@ class ExpenseForm extends React.Component {
   
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        <form  className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
             type="text"
+            className="text-input"
             placeholder="Description"
             autoFocus
             value={this.state.description}
@@ -69,6 +69,7 @@ class ExpenseForm extends React.Component {
           />
           <input
             type="text"
+            className="text-input"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
@@ -84,12 +85,14 @@ class ExpenseForm extends React.Component {
           <textarea
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
+            className="textarea"
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>
+          </div>
         </form>
-      </div>
     )
   }
 }
